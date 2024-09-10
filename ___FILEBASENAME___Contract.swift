d184d07ec3e8c5321af0d78a7533346b8baf7454
Metehan Belli <metehanbelli8@gmail.com>
2024-09-10 11:43:57 +0300

    Contract File added.

---
+++ ___FILEBASENAME___Contract.swift
@@ -0,0 +1,39 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+
+// MARK: - Module Input Protocol
+protocol ___VARIABLE_productName___ModuleInputProtocol: AnyObject {
+    // Define input methods to pass data into the module from another module
+}
+
+// MARK: - Module Output Protocol
+protocol ___VARIABLE_productName___ModuleOutputProtocol: AnyObject {
+    // Define output methods to send data/events to another module
+}
+
+// MARK: - View Input Protocol
+protocol ___VARIABLE_productName___ViewInputProtocol: AnyObject {
+    // Define methods to update the view
+}
+
+// MARK: - View Output Protocol
+protocol ___VARIABLE_productName___ViewOutputProtocol: AnyObject {
+    // Define methods to handle view events, like button taps, text changes etc.
+}
+
+// MARK: - Model Protocol
+protocol ___VARIABLE_productName___ModelProtocol: AnyObject {
+    // Define methods for data handling and business logic
+}
+
+// MARK: - Controller Protocol
+protocol ___VARIABLE_productName___ControllerProtocol: AnyObject {
+    var view: ___VARIABLE_productName___ViewInputProtocol? { get set }
+}
