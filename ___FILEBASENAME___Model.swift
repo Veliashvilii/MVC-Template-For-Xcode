0fbe3b3cff26f0f7a7c3dc53e30cc01bcfe86673
Metehan Belli <metehanbelli8@gmail.com>
2024-09-10 12:31:29 +0300

    Model file added.

---
+++ ___FILEBASENAME___Model.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+
+// MARK: - Model Implementation
+class ___VARIABLE_productName___Model {
+    
+}
+
+// MARK: - Extension
+
+extension ___VARIABLE_productName___Model: ___VARIABLE_productName___ModelProtocol {
+    
+}
